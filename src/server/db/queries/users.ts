810d20d162f3c26918127943580946f1e890294f
Dalton Freeman <daltonfreeman@Daltons-MacBook-Pro-2.local>
2020-01-28 15:20:58 -0600

    updating register logic

--- src/server/db/queries/users.ts
+++ src/server/db/queries/users.ts
@@ -1,8 +1,11 @@
 import { Query } from '../index';
-import { TUsers } from '../tables';
+import { TUsers, ObjResponse} from '../tables';
 
 const findEmail = (email: string) => Query<TUsers[]>('SELECT * FROM users WHERE email = ?', [email]);
 
+const register = (user:any) => Query<ObjResponse>("INSERT INTO users SET ?",user)
+
 export default {
-	findEmail
+    findEmail,
+    register
 };
--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -1,24 +1,22 @@
 import * as express from "express";
 import { createToken } from "../../security/tokens";
 import { hashPassword } from "../../security/passwords";
-// import db from "../../db";
+import db from "../../db";
 
 const router = express.Router();
 
 router.post("/", async (req: any, res) => {
   try {
-    // req.body.password = hashPassword(req.body.password);
-    // let result: any = await db.users.register(
-    //   req.body.name,
-    //   req.body.email,
-    //   req.body.password
-    // );
-    // let token = await createToken({ userid: result.insertId });
-    // res.json({
-    //   token,
-    //   userid: result.insertId,
-    //   role: "guest"
-    // });
+    req.body.password = hashPassword(req.body.password);
+    let result = await db.users.register(
+      req.body
+    );
+    let token = await createToken({ userid: result.insertId });
+    res.json({
+      token,
+      userid: result.insertId,
+      role: "guest"
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json("My Code Sucks Let Me Know ");
