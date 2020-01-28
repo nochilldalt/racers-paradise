9a2ed5c088238e4d6c575a804f6165799b710d7a
przekwas <luke@covalence.io>
2020-01-28 15:05:09 -0600

    fixing token queries in utils

--- src/server/security/tokens.ts
+++ src/server/security/tokens.ts
@@ -1,37 +1,37 @@
-import * as jwt from "jsonwebtoken";
-import * as crypto from "crypto";
-// import db from "../../db";
-// import config from "../../config";
+import * as jwt from 'jsonwebtoken';
+import * as crypto from 'crypto';
+import db from '../db';
+import config from "../config";
 
 export const createToken = async (payload: IPayload) => {
-  try {
-    // let result: any = await db.tokens.insert(payload.userid);
-    // payload.tokenid = result.insertId;
-    // payload.unique = crypto.randomBytes(32).toString("hex");
-    // let token = await jwt.sign(payload, config.secret);
-    // await db.tokens.update(token, payload.tokenid);
-    // return token;
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		let result = await db.tokens.insert(payload.userid);
+		payload.tokenid = result.insertId;
+		payload.unique = crypto.randomBytes(32).toString("hex");
+		let token = await jwt.sign(payload, config.secret);
+		await db.tokens.update(token, payload.tokenid, payload.unique);
+		return token;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const validateToken = async (token: string) => {
-   try {
-//     let payload:IPayload = <IPayload>jwt.decode(token)
-//     let [validToken]:any = await db.tokens.match(token, payload.userid, payload.tokenid)
-//     if (validToken) {
-//       return payload
-//     } else {
-//       return new Error('INVALIED TOKEN')
-//     }
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		let payload: IPayload = <IPayload>jwt.verify(token, config.secret);
+		let [matchedToken] = await db.tokens.match(token, payload.userid, payload.tokenid, payload.unique);
+		if (matchedToken) {
+			return payload;
+		} else {
+			return new Error('INVALIED TOKEN');
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 interface IPayload {
-  userid: number;
-  tokenid?: number;
-  unique?: string;
+	userid: number;
+	tokenid?: number;
+	unique?: string;
 }
