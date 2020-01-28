b3b110d92da0077d6326fcba35d3c1c2e43971b2
przekwas <luke@covalence.io>
2020-01-28 15:13:22 -0600

    adding find one by id query

--- src/server/db/queries/users.ts
+++ src/server/db/queries/users.ts
@@ -3,6 +3,9 @@ import { TUsers } from '../tables';
 
 const findEmail = (email: string) => Query<TUsers[]>('SELECT * FROM users WHERE email = ?', [email]);
 
+const findId = (id: number) => Query<TUsers[]>('SELECT * FROM users WHERE id = ?', [id]);
+
 export default {
-	findEmail
+	findEmail,
+	findId
 };
